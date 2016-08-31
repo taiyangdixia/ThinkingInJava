6a88203387bd499138df56fc644991d03b7089d4
taiyangdixia <shijing_917@126.com>
2016-08-31 16:33:20 +0800

    abstract

---
+++ src/Chapter20/AtUnit.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package Chapter20;
+
+/**
+ * @author eva_shi
+ * not quite understand
+ */
+public class AtUnit{
+
+}
---
+++ src/chapter9/AbstractTest.java
@@ -0,0 +1,21 @@
+package chapter9;
+
+/**
+ * @author eva_shi
+ *
+ */
+public class AbstractTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+abstract class AbstractClass { //包含抽象方法的类必须声明abstract关键字，否则编译器报错
+	abstract void f();
+}
